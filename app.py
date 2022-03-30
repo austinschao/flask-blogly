127fda7699db0b85ebb1fad7b2e564248cf164ba
Austin Chao <austinschao@gmail.com>
2022-03-30 12:17:58 -0700

    Created templates and added User class

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, redirect, request
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret-pw'
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """ Connect to database """
+
+    db.app = app
+    db.init_app(app)
+
+
+
 """Models for Blogly."""
+
+class User(db.Model):
+    """ Creating a User """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,
+                    primary_key = True,
+                    autoincrement = True)
+
+    first_name = db.Column(db.String(50),
+                            nullable = False)
+
+    last_name = db.Column(db.String(50),
+                            nullable = False)
+
+    image_url = db.Column(db.Text,
+                            nullable = True)
+
